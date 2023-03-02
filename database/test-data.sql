e614d5545f6117d2a153b9b504479359b57d2991
Iris Bromley <iris.j.bromley@gmail.com>
2023-03-02 22:02:37 +0100

    Add Database and tables

---
+++ database/test-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ('Joe', 'Bloggs', 'joe@test.com', '');
+
+
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ('Minnie', 'Mouse', 'minniemouse@test.com', '');
+
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ('Wim', 'Wenders', 'wim@test.com', '');
+
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ('Stanley', 'Kubrick', 'stan@test.com', '');
+
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ('Mark', 'Bellamy', 'muse@test.com', '');
+
+
