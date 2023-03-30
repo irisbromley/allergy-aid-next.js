a18f91cefff6ea81f520c08ca4b47e536247ae13
Iris Bromley <iris.j.bromley@gmail.com>
2023-03-30 20:38:26 +0200

    Add router refresh to editdailylogpage

--- app/daily-log/[personID]/edit/[dailyLogID]/page.tsx
+++ app/daily-log/[personID]/edit/[dailyLogID]/page.tsx
@@ -1,5 +1,5 @@
 import { Cousine } from 'next/font/google';
-import { notFound } from 'next/navigation';
+import { notFound, useRouter } from 'next/navigation';
 import { getDailyLogByID } from '../../../../../database/daily-logs';
 import { getPersonByID } from '../../../../../database/users';
 import DailyLogForm from '../../new/DailyLogForm';
@@ -10,10 +10,13 @@ export default async function EditDailyLogPage({ params }: Props) {
   const dailyLog = await getDailyLogByID(+params.dailyLogID, +params.personID);
   const person = await getPersonByID(+params.personID);
   console.log(dailyLog);
+  const router = useRouter();
 
   if (!dailyLog || !person) {
     notFound();
   }
+  router.refresh();
+
   return (
     <main>
       <div className="w-full max-w-md md:max-w-lg mx-auto">
